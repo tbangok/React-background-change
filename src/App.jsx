31d6b3ad16a59313a2393202ae1211c378e6e2d3
anhngoc <tabaoanhngoc@gmail.com>
2024-03-07 16:09:37 +0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const array = [R,G,B]
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div  style={{ 
+      height:'100vh',
+      width:'100vw',  
+      display:'flex',
+      backgroundColor:'salmon'
+    }} >
+      
+     <button className="random" style={{margin:'auto'}}> Random Color</button>
+    </div>
   )
 }
 
